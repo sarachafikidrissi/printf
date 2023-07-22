e2489c50ae62949ae839dbd9378324cdcd46b1e9
chafikidrissisara <chafikidrissisara@gmail.com>
2023-07-22 18:01:54 +0100

    new updates

--- main.h
+++ main.h
@@ -4,7 +4,11 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int _printf(const char *format, ...);
+int print_char(va_list args);
+int _putchar(char c);
+
 
 #endif
--- myprintf.c
+++ myprintf.c
@@ -1,11 +1,31 @@
 #include "main.h"
-/**
- * 
-*/
+#include <unistd.h>
+
 int _printf(const char *format, ...)
 {
-    unsigned int i;
     va_list args;
+    unsigned int count = 0, i = 0;
+    char c;
+
     va_start(args, format);
 
+    for (i = 0; format[i] != '\0'; i++)
+    {
+        if (format[i] == '%')
+        {
+            if (format[i + 1] == 'c')
+            {
+               print_char(args); 
+                count ++;
+            }
+            /*
+            else if (format[i + 1] == 's')
+            {
+                print_string(va_arg(args, char *));
+                count++;
+            }*/
+        }
+    }
+    va_end(args);
+    return (count);
 }
