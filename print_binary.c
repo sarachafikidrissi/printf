edd7f318823371059f85cf8f827762c7ebeb6149
chafikidrissisara <chafikidrissisara@gmail.com>
2023-07-25 16:24:32 +0100

    fixing some errors

--- print_binary.c
+++ print_binary.c
@@ -12,14 +12,14 @@ int print_binary(va_list args)
 	for (j = 1; j < 32; j++)
 	{
 		m /= 2;
-		a[j] = (decimal / m) % 2;
+		array[j] = (decimal / m) % 2;
 	}
 	for (j = 0; j < 32; j++)
 	{
 		limit += array[j];
 		if (limit || j == 31)
 		{
-			_putchar(a[j] + '0');
+			_putchar(array[j] + '0');
 			count++;
 		}
 	}
